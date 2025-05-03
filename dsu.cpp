d909b06e7b04ad5b6149407a5b58a3ad2887279f
Biraj Roy <birajroy0007@gmail.com>
2025-05-03 15:18:50 +0530

    Create dsu.cpp
    dsu

---
+++ dsu.cpp
@@ -0,0 +1,41 @@
+class DSU {
+    vector<int> parent, rank, size;
+public:
+    DSU(int n) {
+        parent.resize(n + 1);
+        rank.resize(n + 1, 0);
+        size.resize(n + 1, 1);
+        for (int i = 0; i <= n; ++i)
+            parent[i] = i;
+    }
+
+    int find(int u) {
+        if (parent[u] != u)
+            parent[u] = find(parent[u]);
+        return parent[u];
+    }
+
+    void unionByRank(int u, int v) {
+        int pu = find(u), pv = find(v);
+        if (pu == pv) return;
+        if (rank[pu] < rank[pv]) {
+            parent[pu] = pv;
+        } else if (rank[pu] > rank[pv]) {
+            parent[pv] = pu;
+        } else {
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+    }
+    void unionBySize(int u, int v) {
+        int pu = find(u), pv = find(v);
+        if (pu == pv) return;
+        if (size[pu] < size[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        } else {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+    }
+};
