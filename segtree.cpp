b4c3a1c4f077f7286b8b5122e2b0c9335ed41df9
Biraj Roy <birajroy0007@gmail.com>
2025-11-24 00:09:36 +0530

    zero base segTree

--- segtree.cpp
+++ segtree.cpp
@@ -1,6 +1,3 @@
- #include <bits/stdc++.h>
-using namespace std;
-
 template <typename NodeT>
 struct SegTree {
     int n;
@@ -13,14 +10,15 @@ struct SegTree {
     SegTree(int _n,
             function<NodeT(const NodeT&, const NodeT&)> _merge,
             NodeT _idNode)
-        : n(_n), st(4 * _n, _idNode), merge(_merge), idNode(_idNode) {}
+        : n(_n), st(4 * _n, _idNode),
+          merge(_merge), idNode(_idNode) {}
 
     SegTree(const vector<NodeT>& arr,
             function<NodeT(const NodeT&, const NodeT&)> _merge,
             NodeT _idNode)
-        : n((int)arr.size()), st(4 * arr.size(), _idNode),
+        : n(arr.size()), st(4 * arr.size(), _idNode),
           merge(_merge), idNode(_idNode) {
-        build(1, 0, n - 1, arr);
+        build(0, 0, n - 1, arr);
     }
 
     void build(int p, int l, int r, const vector<NodeT>& arr) {
@@ -28,14 +26,16 @@ struct SegTree {
             st[p] = arr[l];
             return;
         }
-        int m = (l + r) / 2;
-        build(p << 1, l, m, arr);
-        build(p << 1 | 1, m + 1, r, arr);
-        st[p] = merge(st[p << 1], st[p << 1 | 1]);
+        int m = (l + r) >> 1;
+        int L = 2 * p + 1;
+        int R = 2 * p + 2;
+        build(L, l, m, arr);
+        build(R, m + 1, r, arr);
+        st[p] = merge(st[L], st[R]);
     }
 
     void update(int idx, const NodeT& val) {
-        update(1, 0, n - 1, idx, val);
+        update(0, 0, n - 1, idx, val);
     }
 
     void update(int p, int l, int r, int idx, const NodeT& val) {
@@ -43,23 +43,27 @@ struct SegTree {
             st[p] = val;
             return;
         }
-        int m = (l + r) / 2;
-        if (idx <= m)
-            update(p << 1, l, m, idx, val);
-        else
-            update(p << 1 | 1, m + 1, r, idx, val);
-        st[p] = merge(st[p << 1], st[p << 1 | 1]);
+        int m = (l + r) >> 1;
+        int L = 2 * p + 1;
+        int R = 2 * p + 2;
+
+        if (idx <= m) update(L, l, m, idx, val);
+        else          update(R, m + 1, r, idx, val);
+
+        st[p] = merge(st[L], st[R]);
     }
 
     NodeT query(int L, int R) {
-        return query(1, 0, n - 1, L, R);
+        return query(0, 0, n - 1, L, R);
     }
 
     NodeT query(int p, int l, int r, int L, int R) {
         if (R < l || r < L) return idNode;
         if (L <= l && r <= R) return st[p];
-        int m = (l + r) / 2;
-        return merge(query(p << 1, l, m, L, R),
-                     query(p << 1 | 1, m + 1, r, L, R));
+
+        int m = (l + r) >> 1;
+        int left = query(2*p + 1, l, m, L, R);
+        int right = query(2*p + 2, m + 1, r, L, R);
+        return merge(left, right);
     }
 };
